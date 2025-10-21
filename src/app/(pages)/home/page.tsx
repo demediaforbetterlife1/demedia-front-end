9695446b7259392a2f5286d8dc9ef2dc9dfdcaf3
demediaforbetterlife1 <demediaforbetterlife1@gmail.com>
2025-10-21 08:54:30 +0300

    Update page.tsx

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
     return (
         <div className={`min-h-screen ${themeClasses.bg} ${themeClasses.text} flex flex-col pb-20 md:pb-0`}>
             <Stories />
-            <Posts postId={resolvedParams.id} isVisible={true} />
+            <Posts isVisible={true} />
         </div>
     )
 };
