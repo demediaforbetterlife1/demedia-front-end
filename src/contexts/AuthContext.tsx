6f9aa56d8b9a7e95183cb0d783170ad6df42cf64
demediaforbetterlife1 <demediaforbetterlife1@gmail.com>
2025-11-02 13:37:50 +0200

    update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -7,7 +7,7 @@ import { apiFetch } from "@/lib/api";
 import { useI18n } from "@/contexts/I18nContext";
 
 interface User {
-  id: string |number;
+  id: string | number;
   name: string;
   email: string;
   username: string;
@@ -44,7 +44,7 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const useAuth = () => {
   const context = useContext(AuthContext);
-  if (!context) throw new Error("useAuth must be used within an AuthProvider");
+  if (!context) throw new Error("useAuth must be used within an AuthProvider!!");
   return context;
 };
 
