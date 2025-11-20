de0abbcc295ea719606de53d149d5d59de3f1fc9
demediaforbetterlife1 <demediaforbetterlife1@gmail.com>
2025-11-20 11:40:45 +0200

    fixes

--- next.config.mjs
+++ next.config.mjs
@@ -2,28 +2,36 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-  
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  
-   // output: "standalone",
-    compress: true,
-    poweredByHeader: false,
-  
-    async rewrites() {
-        const target = "https://demedia-backend.fly.dev";
-        return [
-          // API routes only
-          {
-            source: "/socket.io/:path*",
-            destination: `${target}/socket.io/:path*`,
-          },
-          // تأكد إن باقي الملفات (زي _next/static) ما تتأثرش
-        ];
-      }      
-  };
+  reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
+  // output: "standalone",
+  compress: true,
+  poweredByHeader: false,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "demedia-backend.fly.dev",
+        pathname: "/uploads/**",
+      },
+    ],
+  },
+
+  async rewrites() {
+    const target = "https://demedia-backend.fly.dev";
+    return [
+      {
+        source: "/socket.io/:path*",
+        destination: `${target}/socket.io/:path*`,
+      },
+    ];
+  },
+};
   
   export default nextConfig;
   
